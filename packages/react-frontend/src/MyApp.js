e83f6fdd8da9423268b27cbf731c2af95475d075
Shravan Sheth <shravansheth@Shravans-Mac-mini.local>
2024-01-17 21:56:52 -0800

    that was not the whole assgn. Progress commit

--- packages/react-frontend/src/MyApp.js
+++ packages/react-frontend/src/MyApp.js
@@ -1,28 +1,12 @@
 // src/MyApp.js
 import React, { useState } from "react";
 import Table from "./Table";
+import Form from "./Form";
 
 
 
 function MyApp() {
-  const [characters, setCharacters] = useState([
-    {
-      name: "Charlie",
-      job: "Janitor"
-    },
-    {
-      name: "Mac",
-      job: "Bouncer"
-    },
-    {
-      name: "Dee",
-      job: "Aspring actress"
-    },
-    {
-      name: "Dennis",
-      job: "Bartender"
-    }
-  ]);
+  const [characters, setCharacters] = useState([]);
   function removeOneCharacter(index) {
     const updated = characters.filter((character, i) => {
       return i !== index;
@@ -36,6 +20,16 @@ function MyApp() {
           characterData={characters}
           removeCharacter={removeOneCharacter}
         />
+    </div>
+  );
+
+  return (
+    <div className="container">
+      <Table
+        characterData={characters}
+        removeCharacter={removeOneCharacter}
+      />
+      <Form />
     </div>
   );
 }
