26f0985883f1958abaf51a8119543cf1f02be915
Shravan Sheth <shravansheth@Shravans-Mac-mini.local>
2024-01-30 00:02:29 -0800

    finished ex2

--- packages/express-backend/index.js
+++ packages/express-backend/index.js
@@ -39,9 +39,25 @@ const findUserByName = (name) => {
   );
 };
 
+const findUserByNameAndJob = (name, job) => {
+  return users["users_list"].filter(
+    (user) => (!name || user["name"] === name) && (!job || user["job"] === job)
+  );
+};
+
+
 const addUser = (user) => {
   users["users_list"].push(user);
   return user;
+};
+
+const deleteUserById = (id) => {
+  const index = users["users_list"].findIndex((user) => user["id"] === id);
+  if (index !== -1) {
+    users["users_list"].splice(index, 1);
+    return true; // Return true if user was found and deleted
+  }
+  return false; // Return false if user was not found
 };
 
 const findUserById = (id) =>
@@ -49,6 +65,8 @@ const findUserById = (id) =>
 
 
 app.use(express.json());
+
+
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
@@ -66,8 +84,10 @@ app.listen(port, () => {
 
 app.get("/users", (req, res) => {
   const name = req.query.name;
+  const job = req.query.job;
+
   if (name != undefined) {
-    let result = findUserByName(name);
+    let result = findUserByNameAndJob(name, job);
     result = { users_list: result };
     res.send(result);
   } else {
@@ -89,4 +109,14 @@ app.post("/users", (req, res) => {
   const userToAdd = req.body;
   addUser(userToAdd);
   res.send();
+});
+
+app.delete("/users/:id", (req, res) => {
+  const id = req.params.id;
+  const wasDeleted = deleteUserById(id);
+  if (wasDeleted) {
+    res.status(200).send(`User with id ${id} was deleted.`);
+  } else {
+    res.status(404).send("User not found.");
+  }
 });
