088f86e7dbeb8a7ad3aa3f1e134ba4b44b985cd1
Shravan Sheth <shravansheth@Shravans-Mac-mini.local>
2024-01-30 20:32:36 -0800

    p3 Ex3

--- packages/react-frontend/src/MyApp.js
+++ packages/react-frontend/src/MyApp.js
@@ -14,8 +14,12 @@ function MyApp() {
     setCharacters(updated);
   }
 
-  function updateList(person) {
-    setCharacters([...characters, person]);
+  function updateList(person) { 
+    postUser(person)
+      .then(() => setCharacters([...characters, person]))
+      .catch((error) => {
+        console.log(error);
+      })
   }
 
   function fetchUsers() {
@@ -28,6 +32,18 @@ function MyApp() {
       .then((json) => setCharacters(json["users_list"]))
       .catch((error) => { console.log(error); });
   }, [] );
+
+  function postUser(person) {
+    const promise = fetch("Http://localhost:8000/users", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(person),
+    });
+
+    return promise;
+  }
 
   // return (
   //   <div className = "container">
